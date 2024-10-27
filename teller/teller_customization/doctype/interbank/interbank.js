0ec2a0123447b8463c5d03aa7453207e6e915551
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-10-27 17:18:50 +0300

    filter on childtable field

--- teller/teller_customization/doctype/interbank/interbank.js
+++ teller/teller_customization/doctype/interbank/interbank.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2024, Mohamed AbdElsabour and contributors
 // For license information, please see license.txt
+frappe.ui.form.on("InterBank", {
+  refresh: function (frm) {
+    frm.add_custom_button(__("Fetch"), function () {
+      frm.call({
+        method: "fetch_data",
+        doc: frm.doc,
+        callback: function (response) {
+          if (response.message) {
+            // Populate your fields here with the fetched data
+            // Example:
+            frm.set_value("field_name", response.message.field_value);
+          }
+        },
+      });
+    });
+  },
+});
 
 // frappe.ui.form.on("InterBank", {
 // 	refresh(frm,cdt,cdn) {
@@ -14,61 +31,81 @@
 //         };
 // 	},
 // });
-
-
-// frappe.ui.form.on("InterBank Details",{
-//     custom_currency_code(frm,cdt,cdn){
-//         var d = locals[cdt][cdn];
-//         frm.fields_dict["InterBank Details"].grid.get_field("currency").get_query =
-//         function () {
-        //   return {
-        //     filters: [
-        //         ['Currency','custom_currency_code','=',d.custom_currency_code]
-           
-        //     ]
-        //   };
-        // };
-
-
-//     }
-
-
-// })
+// frappe.ui.form.on("InterBank", {
+//   refresh: function (frm, cdt, cdn) {
+//     frm.set_query("currency", "InterBank Details", function (doc, cdt, cdn) {
+//       return {
+//         filters: ["name", "!=", "EGP"],
+//       };
+//     });
+//   },
+// });
+// frappe.ui.form.on("InterBank Details", {
+//   custom_currency_code(frm, cdt, cdn) {
+//     var d = locals[cdt][cdn];
+//     frm.fields_dict["InterBank Details"].grid.get_field("currency").get_query =
+//       function () {
+//         return {
+//           filters: ["Currency", "!=", "EGP"],
+//         };
+//       };
+//   },
+// });
+// frappe.ui.form.on("InterBank", "refresh", function (frm) {
+//   frm.fields_dict["InterBank Details"].grid.get_field("currency").get_query =
+//     function (doc, cdt, cdn) {
+//       var child = locals[cdt][cdn];
+//       console.log(child);
+//       return {
+//         filters: [["name", "!=", "EGP"]],
+//       };
+//     };
+// });
+frappe.ui.form.on("InterBank", {
+  refresh: function (frm) {
+    frm.fields_dict["interbank"].grid.get_field("currency").get_query =
+      function (doc, cdt, cdn) {
+        return {
+          filters: {
+            name: ["!=", "EGP"],
+          },
+        };
+      };
+  },
+});
 
 frappe.ui.form.on("InterBank Details", {
-    custom_currency_code(frm,cdt,cdn) {
-        var row = locals[cdt][cdn];
-        console.log("Row data:", row);
-        
-        // Fetch Currency records based on custom_currency_code
-        frappe.call({
-            method: "frappe.client.get_list",
-            args: {
-                doctype: "Currency",
-                fields: ["name", "custom_currency_code"],
-                filters: [
-                    ["custom_currency_code", "=", row.custom_currency_code]
-                ]
-            },
-            callback: function(response) {
-                let currencies = response.message || [];
-                console.log("Fetched currencies:", currencies);
-    
-                // Assuming you need to update something based on these currencies
-                if (currencies.length > 0) {
-                    // Update the form field with the first currency's details as an example
-                    let currency = currencies[0]; // Take the first matched currency
-                    console.log("Selected currency:", currency);
-    
-                    // Example: Update a field in the current row
-                    frappe.model.set_value(cdt, cdn, "currency", currency.name);
-    
-                    // Optionally, you can set additional fields if needed
-                    // frappe.model.set_value(cdt, cdn, "another_field", currency.another_field);
-                } else {
-                    console.log("No matching currencies found.");
-                }
-            }
-        });
-    }
+  custom_currency_code(frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+    console.log("Row data:", row);
+
+    // Fetch Currency records based on custom_currency_code
+    frappe.call({
+      method: "frappe.client.get_list",
+      args: {
+        doctype: "Currency",
+        fields: ["name", "custom_currency_code"],
+        filters: [["custom_currency_code", "=", row.custom_currency_code]],
+      },
+      callback: function (response) {
+        let currencies = response.message || [];
+        console.log("Fetched currencies:", currencies);
+
+        // Assuming you need to update something based on these currencies
+        if (currencies.length > 0) {
+          // Update the form field with the first currency's details as an example
+          let currency = currencies[0]; // Take the first matched currency
+          console.log("Selected currency:", currency);
+
+          // Example: Update a field in the current row
+          frappe.model.set_value(cdt, cdn, "currency", currency.name);
+
+          // Optionally, you can set additional fields if needed
+          // frappe.model.set_value(cdt, cdn, "another_field", currency.another_field);
+        } else {
+          console.log("No matching currencies found.");
+        }
+      },
+    });
+  },
 });
--- teller/teller_customization/doctype/interbank/interbank.py
+++ teller/teller_customization/doctype/interbank/interbank.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2024, Mohamed AbdElsabour and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class InterBank(Document):
-	pass
+    @frappe.whitelist()
+    def fetch_data(self):
+        # Your logic to fetch data
+        sql = """
+		   SELECT
+            account,
+            SUM(credit_in_account_currency) AS sum_currency_sale,
+            SUM(debit_in_account_currency) AS sum_currency_purchase,
+            account_currency,
+            SUM(credit) AS sum_egy_sale,
+            SUM(debit) AS sum_egy_purchase,
+            posting_date
+        FROM
+            `tabGL Entry`
+        WHERE
+            account IN (SELECT name FROM `tabAccount` WHERE account_type = 'Cash')
+
+		"""
+
+        sr = frappe.db.sql(sql, as_dict=True)
+        fetched_data = sr  # Replace with actual fetched data
+        return fetched_data
