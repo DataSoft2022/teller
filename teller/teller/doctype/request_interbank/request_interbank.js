337c1be8885aaf78b1570bf90ae45699be55a848
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2024-11-20 16:11:09 +0200

    fea: change trigger to on_submit

--- teller/teller/doctype/request_interbank/request_interbank.js
+++ teller/teller/doctype/request_interbank/request_interbank.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Request interbank", {
-  after_save: function (frm) {
+  on_submit: function (frm) {
       frm.events.create_booking(frm);
   },
   create_booking: function (frm) {
@@ -26,7 +26,7 @@ frappe.ui.form.on("Request interbank", {
 frappe.ui.form.on('Interbank Request Details', {
 	curency_code(frm, cdt, cdn) {
     var row = locals[cdt][cdn];
-    console.log("Row data:", row);
+    // console.log("Row data:", row);
 
     // Fetch Currency records based on custom_currency_code
     frappe.call({
@@ -38,13 +38,13 @@ frappe.ui.form.on('Interbank Request Details', {
       },
       callback: function (response) {
         let currencies = response.message || [];
-        console.log("Fetched currencies:", currencies);
+        // console.log("Fetched currencies:", currencies);
 
         // Assuming you need to update something based on these currencies
         if (currencies.length > 0) {
           // Update the form field with the first currency's details as an example
           let currency = currencies[0]; // Take the first matched currency
-          console.log("Selected currency:", currency);
+          // console.log("Selected currency:", currency);
 
           // Example: Update a field in the current row
           frappe.model.set_value(cdt, cdn, "currency", currency.name);
