fb43e3ebf8dc0c462a150e11a483a72dfa763b97
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-01-05 11:14:29 +0200

    feat:Close interbank Daily at 23:59:59

--- teller/validate_time.py
+++ teller/validate_time.py
@@ -2,18 +2,18 @@ import frappe
 from frappe.utils.data import now, get_datetime
 from frappe.utils import nowtime
 from datetime import datetime
+@frappe.whitelist()
 def cron_validate_interbank_time():
     cur_time = get_datetime(now()).strftime('%H:%M:%S')
     cur_day = get_datetime(now()).date()
     stopWatch = f"23:59:59"
     print(f"time {cur_time} ad cur day is {cur_day}")
 
-    open_interbanks = frappe.get_all('InterBank', filters={'status': 'Open', 'type':'Daily','date':cur_day}, fields=['name','date'])
+    open_interbanks = frappe.get_all('InterBank', filters={'status': ['!=', 'Close'], 'type':'Daily','date':cur_day}, fields=['name','date','status','type'])
     for interbank in open_interbanks:
-        print(f"interbank {interbank}")
-        print(f"type cur_time {type(cur_time)},, type stopWatch {type(stopWatch)}")
-        if cur_time == stopWatch:
-            frappe.msgprint(f" function is good") 
-        frappe.db.set_value('InterBank', interbank.name, 'status', 'Ended')
-
-    # frappe.db.commit()      
+        # return cur_time >= stopWatch  and interbank.date == cur_day
+        if cur_time == stopWatch and interbank.date == cur_day:
+            # frappe.msgprint(f" function is good")
+          ib = frappe.get_doc("InterBank",interbank.name,update_modified=True)
+          ib.db_set('status', 'Ended')  
+          return "yes"
