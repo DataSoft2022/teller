d701dbd736803cdd9722c49a114ea57fa2a7b5e9
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-01-19 13:36:03 +0200

    Validation Emppty table and rate

--- teller/teller/doctype/branch_interbank_request/branch_interbank_request.py
+++ teller/teller/doctype/branch_interbank_request/branch_interbank_request.py
@@ -1,9 +1,17 @@
 # Copyright (c) 2025, Mohamed AbdElsabour and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BranchInterbankRequest(Document):
-	pass
+    def on_submit(self):
+      if not self.branch_request_details:
+          frappe.throw("Table is Empty")
+      for row in self.branch_request_details:
+        if not row.qty or row.qty == 0:
+          frappe.throw(f" Row {row.idx}# can't be rate {row.qty}")
+      self.create_booking()
+    def create_booking(self):
+        frappe.msgprint(f"booking....")  
