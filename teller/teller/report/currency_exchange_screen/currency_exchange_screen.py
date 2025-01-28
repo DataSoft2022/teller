e0fb97f829d1f6b69f0159a6472978e2f70c16c8
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-01-28 11:02:55 +0200

    Report :fix width

--- teller/teller/report/currency_exchange_screen/currency_exchange_screen.py
+++ teller/teller/report/currency_exchange_screen/currency_exchange_screen.py
@@ -11,8 +11,8 @@ def execute(filters=None):
 
 def get_columns():
 	 return [
-        {"label": "Currency", "fieldname": "from_currency", "fieldtype": "Data", "width": 150},
-        {"label": "Buying Exchange Rate", "fieldname": "exchange_rate", "fieldtype": "Float", "width": 150},
+        {"label": "Currency", "fieldname": "from_currency", "fieldtype": "Data", "width": 250},
+        {"label": "Buying Exchange Rate", "fieldname": "exchange_rate", "fieldtype": "Float", "width": 250},
         {"label": "Selling Exchange Rate", "fieldname": "custom_selling_exchange_rate", "fieldtype": "Float", "width": 200},
         {"label": "Last Date", "fieldname": "latest_date", "fieldtype": "Datetime", "width": 250},
         
