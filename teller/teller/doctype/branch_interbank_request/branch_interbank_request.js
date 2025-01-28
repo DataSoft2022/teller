7f7eda7fe9b38cc0253cf1f95ee6fecb3f2e64b1
AhmedAbokhatwa <ahmedreda199785@gmail.com>
2025-01-28 09:59:55 +0200

    feat: currency code to get currency name

--- teller/teller/doctype/branch_interbank_request/branch_interbank_request.js
+++ teller/teller/doctype/branch_interbank_request/branch_interbank_request.js
@@ -5,4 +5,36 @@
 // 	refresh(frm) {
 
 // 	},
-// });
+// });Branch Request Details
+frappe.ui.form.on('Branch Request Details', {
+    currency_code(frm, cdt, cdn) {
+    var row = locals[cdt][cdn];
+    frappe.call({
+      method: "frappe.client.get_list",
+      args: {
+        doctype: "Currency",
+        fields: ["name", "custom_currency_code"],
+        filters: [["custom_currency_code", "=", row.currency_code]],
+      },
+      callback: function (response) {
+        let currencies = response.message || [];
+        // console.log("Fetched currencies:", currencies);
+
+        // Assuming you need to update something based on these currencies
+        if (currencies.length > 0) {
+          // Update the form field with the first currency's details as an example
+          let currency = currencies[0]; // Take the first matched currency
+          // console.log("Selected currency:", currency);
+
+          // Example: Update a field in the current row
+          frappe.model.set_value(cdt, cdn, "currency", currency.name);
+
+          // Optionally, you can set additional fields if needed
+          // frappe.model.set_value(cdt, cdn, "another_field", currency.another_field);
+        } else {
+          console.log("No matching currencies found.");
+        }
+      },
+    });
+  },
+})
